7f8d46952820734519b34e8cdcbb3092801d6b44
Aleksi Johansson <aleksi.johansson@wunderkraut.com>
2016-06-12 16:27:07 +0300

    Removed wundertools-py as a submodule. Going to include it directly in this project.

--- setup.py
+++ setup.py
@@ -64,17 +64,17 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    packages=find_packages(),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    py_modules=["wundertools/wundertools"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     # install_requires=['peppercorn'],
+    install_requires=['docker-compose >= 1.7.1, < 2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -101,9 +101,10 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
+    # TODO: Change this to wundertools.main:main and change the file name too.
     entry_points={
         'console_scripts': [
-            'wundertools-py=wundertools.wundertools:main',
+            'wundertools-py=wundertools.main:main',
         ],
     },
 )
